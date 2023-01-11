d5ea783b2eef9040a8a7d9da1c32f77a034e1fb2
Tom <tomuytdehaag@hotmail.com>
2023-01-11 01:06:37 +0100

    Bugfix

--- dedupe.py
+++ dedupe.py
@@ -77,7 +77,7 @@ def find_duplicate_names(
                 if not path.is_file():
                     continue
 
-                if filters and any([f in path.parts for f in filters]):
+                if filters is not None and any([f in path.parts for f in filters]):
                     continue
 
                 key = re.sub(r"\([0-9]\)", "", path.name)
